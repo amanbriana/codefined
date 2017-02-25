35b8b78d1cc72b50f9d32a5339cba9a0bc650d76
EC2 Default User <ec2-user@ip-172-31-31-115.us-west-2.compute.internal>
2017-02-25 16:49:30 +0000

    Updated version of code

--- js/front.js
+++ js/front.js
@@ -261,3 +261,10 @@ $myCarousel.on('slide.bs.carousel', function (e) {
 });
 
 //---------------- End Bootstrap Carousel ---------------------//
+
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+});
